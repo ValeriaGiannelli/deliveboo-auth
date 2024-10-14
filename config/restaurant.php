50ad34b34ca53078a33c6c115045bbabf39a4442
MicheleLafronza <michele.lafronza91@gmail.com>
2024-10-14 17:54:07 +0200

    feat: seeding user, type and restaurant

---
+++ config/restaurant.php
@@ -0,0 +1,44 @@
+<?php
+
+    return [
+        [
+            'name' => 'MaiNaGioia',
+            'address' => 'via Fasulla 123',
+            'piva' => '12345678900',
+            'img' => 'https://media.printables.com/media/prints/731455/images/5721656_1d3f1f02-8b4c-450b-a782-1305f72748e5_509dd245-620e-4db6-a41f-0d8e74e8240e/krusty-burger.png',
+            'description' => 'Un ristorante dove si mangia triste'
+        ],
+
+        [
+            'name' => 'Trattoria Sapuri',
+            'address' => 'via Fasulla 321',
+            'piva' => '12345468900',
+            'img' => 'https://media.printables.com/media/prints/731455/images/5721656_1d3f1f02-8b4c-450b-a782-1305f72748e5_509dd245-620e-4db6-a41f-0d8e74e8240e/krusty-burger.png',
+            'description' => 'Tanta carne di cavallo, poca partita iva'
+        ],
+
+        [
+            'name' => 'Il Musicista',
+            'address' => 'via dei Violini 321',
+            'piva' => '22345468900',
+            'img' => 'https://media.printables.com/media/prints/731455/images/5721656_1d3f1f02-8b4c-450b-a782-1305f72748e5_509dd245-620e-4db6-a41f-0d8e74e8240e/krusty-burger.png',
+            'description' => 'Si suona tanto, si mangia pochissimo'
+        ],
+
+        [
+            'name' => 'Ristorante al Mare',
+            'address' => 'via del Mare 10',
+            'piva' => '22345468999',
+            'img' => 'https://media.printables.com/media/prints/731455/images/5721656_1d3f1f02-8b4c-450b-a782-1305f72748e5_509dd245-620e-4db6-a41f-0d8e74e8240e/krusty-burger.png',
+            'description' => 'Sapore di mare, sapore di sale'
+        ],
+
+        [
+            'name' => 'Panzerottini',
+            'address' => 'via dei Panzerotti 99',
+            'piva' => '22345468999',
+            'img' => 'https://media.printables.com/media/prints/731455/images/5721656_1d3f1f02-8b4c-450b-a782-1305f72748e5_509dd245-620e-4db6-a41f-0d8e74e8240e/krusty-burger.png',
+            'description' => 'Panzerotti baresi a GoGo'
+        ],
+
+    ];
---
+++ database/seeders/RestaurantTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
+use App\Models\User;
+
+class RestaurantTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $restaurants = config('restaurant');
+
+        $id = 1;
+
+        foreach($restaurants as $restaurant){
+            $new_restaurant = new Restaurant();
+            $new_restaurant->name = $restaurant['name'];
+            $new_restaurant->address = $restaurant['address'];
+            $new_restaurant->piva = $restaurant['piva'];
+            $new_restaurant->img = $restaurant['img'];
+            $new_restaurant->description = $restaurant['description'];
+            // $new_restaurant->user_id = User::inRandomOrder()->first()->id;
+            $new_restaurant->user_id = $id;
+            $id++;
+            $new_restaurant->save();
+            // dump($new_restaurant);
+
+        }
+    }
+}
