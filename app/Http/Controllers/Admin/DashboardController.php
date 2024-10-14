c9827b3d9c32e881242c9ef6e6e4a4f0bb9471e6
ValeriaGiannelli <127607654+ValeriaGiannelli@users.noreply.github.com>
2024-10-14 10:02:12 +0200

    Initial commit

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //creo la funzione pubblica index
+    public function index(){
+        // dump('sono la index della dashboard');
+        return view('admin.index');
+    }
+}
