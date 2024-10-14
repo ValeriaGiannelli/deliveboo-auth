aa00ae1a419f20625dc9ef7b05bc1e683136f676
ElonaHoxhaj96 <elona6915@gmail.com>
2024-10-14 10:54:07 +0200

    feat: migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// finito di modificare la dashboard dovò creare un controller per la pagina guest
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+// da eliminare perché vogliamo chiamarle admin
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+//
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// raggruppo le rotte
+Route::middleware(['auth', 'verified'])
+    // il prefisso deve avere admin
+    ->prefix('admin')
+    // tutti i name devono iniziare con admin.
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+    });
 
 require __DIR__.'/auth.php';
