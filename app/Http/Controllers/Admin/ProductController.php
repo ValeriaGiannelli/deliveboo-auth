c584ceefc42cdbf20e4ca443bf577e76f7d7f6bb
MicheleLafronza <michele.lafronza91@gmail.com>
2024-10-15 14:16:02 +0200

    feat: add if else a restaurant id exists

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -17,14 +17,21 @@ class ProductController extends Controller
     {
         // prendo l'id dell'user
         $user_id = Auth::id();
-
+        
         // prendo l'id del ristorante associato all'id dell'user
         $restaurant_id = Restaurant::where('user_id', $user_id)->value('id');
 
-        // lista di piatti associati all'id del ristorante
-        $products = Product::orderBy('id')->where('restaurant_id', $restaurant_id)->get();
+        if($restaurant_id){
+            // lista di piatti associati all'id del ristorante se esiste
+            $products = Product::orderBy('id')->where('restaurant_id', $restaurant_id)->get();
 
-        return json_decode($products);
+            return json_decode($products);
+        } else {
+            // altrimenti stampo 
+            return ('Prima di vedere i piatti, crea un ristorante');
+        }
+
+        
     }
 
     /**
