da10d59e66f4b9277100b7c7bb073da116d534d5
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-15 16:36:49 +0200

    feat: add route in the side bar

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,13 +1,13 @@
 <aside class="my-4">
     <ul class="list-group">
         <li>
-            <a href="#"><span class="d-md-none d-lg-inline-block">Home</span></a>
+            <a href="{{route('admin.dashboard')}}"><span class="d-md-none d-lg-inline-block">Home</span></a>
         </li>
         <li>
-            <a href="#"><span class="d-md-none d-lg-inline-block">Menu</span></a>
+            <a href="{{route('admin.index')}}"><span class="d-md-none d-lg-inline-block">Menu</span></a>
         </li>
         <li>
-            <a href="#"><span class="d-md-none d-lg-inline-block">Aggiungi piatti</span></a>
+            <a href="{{route('admin.create')}}"><span class="d-md-none d-lg-inline-block">Aggiungi piatti</span></a>
         </li>
     </ul>
 </aside>
