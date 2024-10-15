36e02af77d7f9cfe94acb660e19a83793d000b96
MicheleLafronza <michele.lafronza91@gmail.com>
2024-10-15 10:01:01 +0200

    feat: add sales config and sales seeder

---
+++ database/seeders/SalesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Sale;
+
+class SalesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sales = config('sales');
+
+        foreach($sales as $sale){
+            $new_sale = new Sale();
+            $new_sale->full_name = $sale['full_name'];
+            $new_sale->email = $sale['email'];
+            $new_sale->address = $sale['address'];
+            $new_sale->total_price = $sale['total_price'];
+            $new_sale->phone_number = $sale['phone_number'];
+            $new_sale->save();
+
+        }
+    }
+}
