2ea4942198e930657f487649a17c1e4626ee1c0b
FraCuboni <fracuboni@gmail.com>
2024-10-16 10:52:39 +0200

    feat: add fontawesome cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
