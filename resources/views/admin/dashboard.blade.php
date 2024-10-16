df2b3fd2524e1276c814442339e99451a82d8043
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-10-16 11:02:47 +0200

    add: front-office validation on restaurant name input

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,12 +15,18 @@
             </div>
         @endif --}}
 
-            <form class="row g-3" action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
+            <form class="row g-3" action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data"
+                onsubmit="return validateForm()">
                 @csrf
                 <div class="col-md-6">
-                    <label for="name" class="form-label">Nome del ristorante (*)</label>
+                    <label for="name" class="form-label" id="nameErrorLabel">Nome del ristorante (*)</label>
                     <input type="text" class="form-control {{-- @error('name') is-invalid @enderror --}}" id="name" name="name"
-                        placeholder="Scrivi il nome del ristorante" value="{{ old('name') }}">
+                        placeholder="Scrivi il nome del ristorante" value="{{ old('name') }}" required>
+                    {{-- Errori front-office --}}
+                    <div class="tooltip-error" id="nameTooltip">Il Nome del ristorante è obbligatorio e deve avere almeno 2
+                        caratteri.</div>
+                    <small class="text-danger" id="nameError"></small>
+                    {{-- Errorri back-office --}}
                     {{-- se esiste l'errore name stampa un messaggio anche sotto l'input --}}
                     {{-- @error('name')
                     <small class="text-danger"> {{ $message }} </small>
@@ -31,16 +37,16 @@
                 <div class="col-md-6">
                     <label for="address" class="form-label">Indirizzo (*)</label>
                     <input type="text" class="form-control {{-- @error('address') is-invalid @enderror --}}" id="address" name="address"
-                        placeholder="Inserisci l'indirizzo" value="{{ old('address') }}">
+                        placeholder="Inserisci l'indirizzo" value="{{ old('address') }}" required>
 
                     {{-- @error('address')
                     <small class="text-danger"> {{ $message }} </small>
                 @enderror --}}
                 </div>
                 <div class="col-md-6">
-                    <label for="piva" class="form-label">P.Iva (*)</label>
+                    <label for="piva" class="form-label">P.Iva (11 caratteri)</label>
                     <input type="text" class="form-control {{-- @error('piva') is-invalid @enderror --}}" id="piva" name="piva"
-                        placeholder="Inserisci la tua P.Iva" value="{{ old('piva') }}">
+                        placeholder="Inserisci la tua P.Iva" value="{{ old('piva') }}" required pattern="\d{11}">
 
                     {{-- @error('piva')
                     <small class="text-danger"> {{ $message }} </small>
@@ -50,7 +56,8 @@
                 {{-- caricamento img --}}
                 <div class="col-12">
                     <label for="img" class="form-label">Immagine</label>
-                    <input type="file" name="img" id="img" class="form-control" onchange="showImg(event)">
+                    <input type="file" name="img" id="img" class="form-control" onchange="showImg(event)"
+                        required>
 
                     {{-- anteprima dell'immagine caricata --}}
                     {{-- <img src="/img/no_img.jpg" class="thumb-mini" id="thumb"> --}}
@@ -62,7 +69,7 @@
                 <div class="col-12">
                     <label for="description" class="form-label">Descrizione del ristorante</label>
                     <textarea class="form-control" name="description" id="description" cols="30" rows="10"
-                        placeholder="Descrivi il tuo ristorante">{{ old('description') }}</textarea>
+                        placeholder="Descrivi il tuo ristorante" required>{{ old('description') }}</textarea>
                 </div>
 
                 {{-- chechbox per le Tipologia di ristorante --}}
@@ -77,7 +84,7 @@
 
                 </div>
                 <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="submit" class="btn btn-primary" id="submitBtn" disabled>Invia</button>
                 </div>
                 <div class="col-12">
                     <button type="reset" class="btn btn-primary">Cancella</button>
@@ -87,6 +94,59 @@
 
         {{-- funzioni --}}
         <script>
+            //VALIDAZIONE FORM
+            function validateForm() {
+                let valid = true;
+                //prendo elementi in pagina
+                const nameError = document.getElementById('nameError');
+                const nameErrorLabel = document.getElementById('nameErrorLabel');
+                const nameInput = document.getElementById('name');
+
+                // validazione nome
+                const name = document.getElementById('name').value;
+                if (name.length < 2) {
+                    nameError.innerHTML = "Il Nome del ristorante deve avere almeno due caratteri.";
+                    nameErrorLabel.style = "color:red";
+                    nameInput.style = "border-color:red";
+                    valid = false;
+                }
+
+                return valid;
+            }
+            //Listener per i campi
+            document.getElementById('name').addEventListener('input', checkForm);
+            //Check campi
+            function checkForm() {
+                let valid = true;
+                const name = document.getElementById('name').value;
+                const nameError = document.getElementById('nameError');
+
+                //controllo campo nome
+                if (!name.length > 0) {
+                    nameError.innerHTML = "Il Nome del ristorante è obbligatorio";
+                    valid = false;
+                }
+                document.getElementById('submitBtn').disabled = !valid;
+            }
+
+            /* //Listener su click
+            document.getElementById('name').addEventListener('input', checkInput);
+
+            function checkInput() {
+                let valid = true;
+                const name = document.getElementById('name').value;
+                const nameError = document.getElementById('nameError');
+
+                //controllo campo nome
+                if (!name.length > 0) {
+                    nameError.innerHTML = "Il Nome del ristorante è obbligatorio";
+                    valid = false;
+                } else {
+                    nameError.innerHTML = "";
+                }
+            } */
+
+
             // funzione che cambia l'anteprima del file caricato
             function showImg(event) {
                 const thumb = document.getElementById('thumb');
