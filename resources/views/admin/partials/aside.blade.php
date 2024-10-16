7a80376e87ecd562da2fe96fad2210daa70824a9
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-10-16 21:22:50 +0200

    fix: aside li not displaying

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,17 +1,17 @@
 <aside class="my-3">
     <ul class="list-group navbar-nav">
         <li class="{{ Route::is('admin.restaurants.index') ? 'active' : '' }}">
-            <a href="{{ route('home') }}"><span class="d-md-none d-lg-inline-block">Home</span></a>
+            <a href="{{ route('home') }}"><span>Home</span></a>
         </li>
         <li class="{{ Route::is('admin.products.index') ? 'active' : '' }}">
-            <a href="{{ route('admin.products.index') }}"><span class="d-md-none d-lg-inline-block">Menu</span></a>
+            <a href="{{ route('admin.products.index') }}"><span>Menu</span></a>
         </li>
         <li class="{{ Route::is('admin.products.create') ? 'active' : '' }}">
-            <a href="{{ route('admin.products.create') }}"><span class="d-md-none d-lg-inline-block">Aggiungi
+            <a href="{{ route('admin.products.create') }}"><span>Aggiungi
                     piatti</span></a>
         </li>
         <li class="{{ Route::is('admin.sales.index') ? 'active' : '' }}">
-            <a href="{{ route('admin.sales.index') }}"><span class="d-md-none d-lg-inline-block">Gestione
+            <a href="{{ route('admin.sales.index') }}"><span>Gestione
                     Ordini</span></a>
         </li>
     </ul>
