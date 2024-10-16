a6ec350e0e933e54079d2fd7f318f3c3d39bb022
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-16 15:37:13 +0200

    feat: add edit button route and img placeholder

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -11,14 +11,14 @@
                 <img
                 class="img-fluid"
                 alt="{{ $product->name}}"
-                src="{{asset('storage/' . $product->img)}}">
+                src="{{asset('storage/' . $product->img)}}" onerror="this.src='{{asset('storage/uploads/no_img.jpg')}}'">
             </div>
         </li>
         <li>Prezzo: {{ $product->price }}</li>
         <li> Mostra: {!! $product->visible ? '<span class="text-success">SI</span>' : '<span class="text-danger">NO</span>' !!}</li>
     </ul>
 
-    <a href="#" class="btn btn-secondary">Modifica</a>
+    <a href="{{route('admin.products.edit', $product)}}" class="btn btn-secondary">Modifica</a>
     <form action="{{ route('admin.products.destroy', $product) }}" method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare questo prodotto?');" style="display: inline;">
         @csrf
         @method('DELETE')
