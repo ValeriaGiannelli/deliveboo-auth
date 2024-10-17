256b7727106b1a8c2576ad8c323ea91857010358
MicheleLafronza <michele.lafronza91@gmail.com>
2024-10-17 11:55:28 +0200

    feat: change card to table menu

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -4,54 +4,57 @@
         @auth
             @include('admin.partials.aside')
         @endauth
-        <div class="container-fluid">
-            <div class="row">
-                @if (count($products))
-                    @foreach ($products as $product)
-                        <div class="col-sm-4 col-md-3 col-lg-2">
-                            @if ($product->visible)
-                                <div class="card">
-                                @else
-                                    <div class="card hidden">
-                            @endif
-                            <img src="{{ asset('storage/' . $product->img) }}" class="card-img-top" alt="{{ $product->name }}"
-                                onerror="this.src='{{ asset('storage/uploads/no_img.jpg') }}'">
-                            <div class="card-body d-flex flex-column justify-content-between">
-                                <div class="card-info">
-                                    <h5 class="card-title">{{ $product->name }}</h5>
-                                    <p class="card-text">
-                                        {{ $product->ingredients_descriptions }}
-                                    </p>
-                                    <p>
-                                        Prezzo: {{ $product->price }}&euro;
-                                    </p>
-                                    <p>
-                                        Mostra: {!! $product->visible ? '<span class="text-success">SI</span>' : '<span class="text-danger">NO</span>' !!}
+        @if (count($products))
+            <div class="container-fluid my-3">
+                <a href="{{ route('admin.products.create') }}" class="btn btn-warning ms-5 my-3">Aggiungi Nuovo Piatto</a>
 
-                                    </p>
-                                </div>
-                                <div class="buttons">
-                                    <a href="{{ route('admin.products.show', $product) }}"class="btn btn-primary"><i
-                                            class="fa-solid fa-eye"></i></a>
-                                    <a href="{{ route('admin.products.edit', $product) }}" class="btn btn-warning"><i
-                                            class="fa-solid fa-pen"></i></a>
+                <table class="table ms-5">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome Piatto</th>
+                            <th scope="col">Ingredienti/descrizione</th>
+                            <th scope="col">Immagine</th>
+                            <th scope="col">Prezzo</th>
+                            <th scope="col">Disponibile</th>
+                            <th scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                            <tr>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->ingredients_descriptions }}</td>
+                                <td><img class="thumb-mini" src="{{ asset('storage/' . $product->img) }}"  alt="{{ $product->name }}"
+                                    onerror="this.src='{{ asset('storage/uploads/no_img.jpg') }}'"></td>
+                                <td>{{ $product->price }}&euro;</td>
+                                <td>{{ $product->visible? 'Si' : 'No' }}</td>
+                                <td>
+                                    <a class="btn btn-warning" href="{{route('admin.products.edit', $product)}}">
+                                            Modifica
+                                    </a>
                                     <form action="{{ route('admin.products.destroy', $product) }}" method="POST"
-                                        onsubmit="return confirm('Sei sicuro di voler eliminare questo prodotto?');"
-                                        style="display: inline;">
+                                        onsubmit="return confirm('Sei sicuro di voler eliminare questo prodotto?');" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-danger"><i
-                                                class="fa-solid fa-trash"></i></i></button>
+                                        <button type="submit" class="btn btn-danger">Elimina</button>
                                     </form>
-                                </div>
-                            </div>
-                        </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        
+                    </tbody>
+                </table>
             </div>
-            @endforeach
         @else
             <h2>Non ci sono prodotti</h2>
-            @endif
-        </div>
+
+            <div class="container-fluid my-5">
+                <a href="{{ route('admin.products.create') }}" class="btn btn-warning d-block">Aggiungi Nuovo Piatto</a>
+            </div>
+        @endif
     </div>
-    </div>
+    
 @endsection
+
+{{-- <img src="{{ asset('storage/' . $product->img) }}" class="card-img-top" alt="{{ $product->name }}"
+                                onerror="this.src='{{ asset('storage/uploads/no_img.jpg') }}'"> --}}
