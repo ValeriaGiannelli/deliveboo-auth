a68a57d8dd8ccc4f1c9fbf90f3747f37c7c74306
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-21 10:27:15 +0200

    feat: change title and icon in head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Deliveboo - dashboard</title>
+
+     <link rel="icon" href="{{ asset('images/LOGO.svg') }}" type="image/svg+xml">
 
 
     <!-- Fonts -->
