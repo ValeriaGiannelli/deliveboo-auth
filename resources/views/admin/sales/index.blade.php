f6cd198b6bfff834dcc6a0ce04325cd91ba7bfd9
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-24 11:59:00 +0200

    style: modify col layout of sale index

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -5,39 +5,41 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid d-flex my-4">
-        @auth
-            @include('admin.partials.aside')
-        @endauth
-        <div class="container-fluid">
-            <h1 class="text-center my-3">Lista Ordini</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID Ordine</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Indirizzo</th>
-                        <th scope="col">Prezzo Totale</th>
-                        <th scope="col">Telefono</th>
-                        <th scope="col">Effettuato</th>
-                        <th scope="col">Visualizza</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($sales as $sale)
+        <div class="row w-100">
+            @auth
+                @include('admin.partials.aside')
+            @endauth
+            <div class="col-sm col-12 my-3">
+                <h1 class="text-center my-3">Lista Ordini</h1>
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ $sale->id }}</td>
-                            <td>{{ $sale->full_name }}</td>
-                            <td>{{ $sale->email }}</td>
-                            <td>{{ $sale->address }}</td>
-                            <td>{{ $sale->total_price }}&euro;</td>
-                            <td>{{ $sale->phone_number }}</td>
-                            <td>{{ $sale->created_at->format('d-m-Y H:i') }}</td>
-                            <td><a href="{{ route('admin.sales.show', $sale) }}" class="btn btn-primary">Dettagli</a></td>
+                            <th scope="col">ID Ordine</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col" class="d-none d-lg-table-cell">Email</th>
+                            <th scope="col" class="d-none d-lg-table-cell">Indirizzo</th>
+                            <th scope="col">Prezzo Totale</th>
+                            <th scope="col" class="d-none d-lg-table-cell">Telefono</th>
+                            <th scope="col">Effettuato</th>
+                            <th scope="col">Visualizza</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($sales as $sale)
+                            <tr>
+                                <td>{{ $sale->id }}</td>
+                                <td>{{ $sale->full_name }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $sale->email }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $sale->address }}</td>
+                                <td>{{ $sale->total_price }}&euro;</td>
+                                <td class="d-none d-lg-table-cell">{{ $sale->phone_number }}</td>
+                                <td>{{ $sale->created_at->format('d-m-Y H:i') }}</td>
+                                <td><a href="{{ route('admin.sales.show', $sale) }}" class="btn btn-primary"><i class="fas fa-eye"></a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
