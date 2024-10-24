b0be9d661557c421075361a6c720b27f4c2efaf0
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-10-24 11:10:53 +0200

    Add: mailtrap configuration and mail api and controller

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\Lead;
+use App\Mail\NewContact;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailable\Envelope;
+use Illuminate\Mail\Mailable\Content;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $success = true;
+
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+            ],
+            [
+                'name.required' => 'campo obbligatorio',
+                'email.required' => 'campo obbligatorio'
+            ]
+        );
+
+        if ($validator->fails()) {
+            $success = false;
+            $errors = $validator->errors();
+            return response()->json(compact('success', 'errors'));
+        }
+
+        //salviamo il messaggio nel Db
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        //inviamo la mail
+        Mail::to($new_lead->email)->send(new NewContact($new_lead));
+        return response()->json(compact('success'));
+    }
+}
