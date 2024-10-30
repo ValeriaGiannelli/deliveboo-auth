86b1a13511e379a807a2f545d836c52600c716b2
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-30 18:19:37 +0100

    style: translate the label into italian

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                    <div class="card-header">{{ __('Registrazione') }}</div>
 
                     {{-- se ci sono gli errori stampa un messaggi con gli errori --}}
                     @if ($errors->any())
