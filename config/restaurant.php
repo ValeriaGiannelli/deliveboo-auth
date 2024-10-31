c4e12827943b9a367cdbf66f104ba571f9fd71b2
FraCuboni <fracuboni@gmail.com>
2024-10-31 13:05:23 +0100

    feat: add restaurant img

--- config/restaurant.php
+++ config/restaurant.php
@@ -5,7 +5,7 @@ return [
         'restaurant_name' => 'MaiNaGioia',
         'address' => 'via Fasulla 123',
         'piva' => '12345678900',
-        'img' => '0',
+        'img' => 'uploads/mainagioia.jpg',
         'description' => 'Un ristorante dove si mangia triste'
     ],
 
@@ -13,15 +13,15 @@ return [
         'restaurant_name' => 'Trattoria Sapuri',
         'address' => 'via Fasulla 321',
         'piva' => '12345468900',
-        'img' => '0',
-        'description' => 'Uploads/sapuri.jpg'
+        'img' => 'uploads/sapuri.jpg',
+        'description' => 'Tanta carne di cavallo, poca partita iva'
     ],
 
     [
         'restaurant_name' => 'Il Musicista',
         'address' => 'via dei Violini 321',
         'piva' => '22345468900',
-        'img' => '0',
+        'img' => 'uploads/music.jpg',
         'description' => 'Si suona tanto, si mangia pochissimo'
     ],
 
@@ -29,7 +29,7 @@ return [
         'restaurant_name' => 'Ristorante al Mare',
         'address' => 'via del Mare 10',
         'piva' => '22345468999',
-        'img' => '0',
+        'img' => 'uploads/ristomare.jpg',
         'description' => 'Sapore di mare, sapore di sale'
     ],
 
@@ -37,7 +37,7 @@ return [
         'restaurant_name' => 'Panzerottini',
         'address' => 'via dei Panzerotti 99',
         'piva' => '22345468999',
-        'img' => '0',
+        'img' => 'uploads/panzerottini.jpg',
         'description' => 'Panzerotti baresi a GoGo'
     ],
 
